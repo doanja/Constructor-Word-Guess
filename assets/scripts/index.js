8b552b913d0a87b3a3be5fea3c861279fea4041a
John Doan <doanja@plu.edu>
2019-10-26 18:54:49 -0700

    game logic working and added jsdocs

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,47 @@ const Word = require('./Word');
 const Letter = require('./Letter');
 const Inquirer = require('inquirer');
 
-const wordbank = ['cheese'];
-const validLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-let numberOfGuesses = 10;
+const wordbank = [
+  'feminist',
+  'area',
+  'lonely',
+  'distant',
+  'ballet',
+  'cancer',
+  'wander',
+  'chimney',
+  'gene',
+  'coverage',
+  'dress',
+  'committee',
+  'coup',
+  'project',
+  'shelter',
+  'sound',
+  'scrap',
+  'liberal',
+  'conventional',
+  'host',
+  'posture',
+  'executrix',
+  'size',
+  'desk',
+  'danger',
+  'cabinet',
+  'indulge',
+  'wood',
+  'threat',
+  'mainstream'
+];
 
+const validLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+const MAX_NUMBER_OF_GUESSES = 10;
+let numberOfGuesses = MAX_NUMBER_OF_GUESSES;
+
+/**
+ * function that returns a random word and creates a Word
+ * @param {array} arr array of words
+ */
 const pickWord = (arr = wordbank) => {
   let lettersArr = []; // array to store Letters
   // get a random word
@@ -27,45 +64,69 @@ const pickWord = (arr = wordbank) => {
   return word;
 };
 
+/**
+ * function that handles the game logic and prompts the user for words
+ * @param {object} word the Word object containing the target word to be guessed
+ */
 const promptGuesses = word => {
-  Inquirer.prompt([
-    // Here we create a basic text prompt.
-    {
-      type: 'input',
-      message: 'Guess a Letter!',
-      name: 'letter'
-    }
-  ]).then(res => {
-    const guessedLetter = res.letter.toLowerCase(); // force letter to be lowercase
+  // when no more guesses remaining (lose conditioin)
+  if (numberOfGuesses <= 0) {
+    console.log('YOU LOSE! NO GUESSES REMAINING');
+    initializeGame();
+  }
 
-    // check for non-letter
-    if (!validLetters.includes(guessedLetter)) {
-      console.log('Enter a valid letter...');
-    }
-    // letter is incorrect
-    else if (!word.getUnderlyingCharacters().includes(guessedLetter)) {
-      numberOfGuesses--;
-      console.log('INCORRECT!');
-      console.log(numberOfGuesses + ' remaining...');
-      console.log('Current Word:', word.getWord());
-    }
-    // letter is correct
-    else {
-      word.updateWord(guessedLetter);
-      console.log('Current Word:', word.getWord());
-    }
-  });
+  // when word does not contains underscores (win condition)
+  else if (!word.getWord().includes('_')) {
+    console.log('CORRECT!');
+    initializeGame();
+  }
+
+  // else prompt recursively prompt the user for input
+  else {
+    Inquirer.prompt([
+      // Here we create a basic text prompt.
+      {
+        type: 'input',
+        message: 'Guess a Letter!',
+        name: 'letter'
+      }
+    ]).then(res => {
+      console.log('\n');
+      const guessedLetter = res.letter.toLowerCase(); // force letter to be lowercase
+
+      // check for non-letter
+      if (!validLetters.includes(guessedLetter)) {
+        console.log('Enter a valid letter...');
+      }
+      // letter is incorrect
+      else if (!word.getUnderlyingCharacters().includes(guessedLetter)) {
+        console.log('INCORRECT!');
+        console.log(--numberOfGuesses + ' remaining...');
+        console.log('Current Word:', word.getWord());
+      }
+      // letter is correct
+      else if (word.getUnderlyingCharacters().includes(guessedLetter)) {
+        word.updateWord(guessedLetter);
+        console.log('Current Word:', word.getWord());
+      }
+      promptGuesses(word);
+    });
+  }
 };
 
+/**
+ * function to start the game
+ */
 const initializeGame = () => {
   // pick a word
   const currentWord = pickWord();
 
-  // while there are still blanks... prompt user for letters
-  // while (currentWord.getWord().includes('_') && numberOfGuesses > 0) {
-  //   promptGuesses(currentWord);
-  // }
+  // reset numberOfGuesses
+  numberOfGuesses = MAX_NUMBER_OF_GUESSES;
 
+  console.log('A new game has started...');
+
+  // start prompting for guesses
   promptGuesses(currentWord);
 };
 
